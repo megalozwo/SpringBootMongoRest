96fe3d5a247521dc8c3ab6408c95c4d481746bda
George <gtsigouris@gmail.com>
2016-08-13 09:20:53 +0100

    1st commit - Basic functionality

---
+++ src/main/java/rest/api/dao/UserRepository.java
@@ -0,0 +1,16 @@
+package rest.api.dao;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends MongoRepository<User, String> {
+	
+	List<User> findByLastName(@Param("name") String name);
+	
+
+
+}
